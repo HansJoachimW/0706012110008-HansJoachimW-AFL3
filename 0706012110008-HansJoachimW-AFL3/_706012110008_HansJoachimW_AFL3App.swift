ead3f0dc899879b3bd2d31bc78666caa4ddbebab
HansJoachimW <hansjw123@gmail.com>
2023-04-11 08:25:50 +0700

    Initial Commit

---
+++ 0706012110008-HansJoachimW-AFL3/_706012110008_HansJoachimW_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110008_HansJoachimW_AFL3App.swift
+//  0706012110008-HansJoachimW-AFL3
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110008_HansJoachimW_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
